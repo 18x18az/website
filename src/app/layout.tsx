6817a8dc15bd13b03beb5f9e8041495d2f02cd7f
Alec Miller <alecmichaelmiller@gmail.com>
2023-11-19 11:09:36 -0700

    basic webpage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,9 @@ export default function RootLayout ({
 }): JSX.Element {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        </body>
     </html>
   )
 }
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,17 @@
+import { Link, NavbarProps } from "./interface"
+
+function NavItem(props: Link): JSX.Element {
+    return (
+        <div>
+            {props.name}
+        </div>
+    )
+    
+}
+export default function DesktopNavbar(props: NavbarProps): JSX.Element {
+    const links = props.links.map((link) => <NavItem key={link.href} name={link.name} href={link.href}/>)
+
+    return <div className="hidden xl:flex">
+        {links}
+    </div>
+}
