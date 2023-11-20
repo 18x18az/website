ef1185672fea19f31bd80a58333157c5ce1657b2
Alec Miller <alecmichaelmiller@gmail.com>
2023-11-19 20:14:45 -0700

    include cdn configuration

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public'
 })
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   output: 'standalone',
+  assetPrefix: isProd ? 'https://18x18.azureedge.net' : undefined,
   images: {
     remotePatterns: [
       {
